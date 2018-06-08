9634bd1bf7dbbddf3101f7b1a703bca6734b3148
Zsibrek Krisztian <thaddesu@gmail.com>
2018-06-08 10:45:44 +0200

    working hard

--- script1.py
+++ script1.py
@@ -1,1 +1,8 @@
-print "yo"
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Firefox()
+browser.get("http://bonuszbrigad.hu/bonuszkerek")
+time.sleep(30)
+browser.find_element_by_xpath("//*[@id=startButton]").click()
